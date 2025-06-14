793a43d09733bd95103d9724a9dad3ccd1f32449
faa-rassoul <fatdiopgningue@gmail.com>
2025-06-14 12:16:28 +0100

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -1,6 +1,4 @@
 // directives de compilation djiby
-#include <stdio.h>
-#include <string.h>
 
 
 // fontion addition par marone
@@ -12,18 +10,17 @@
 
 
 // fontion multiplication par djiby
-float multiplication(float a, float b) {
-    return a * b;
-}
 
 
 
 // fontion division par fatou
-
-
-
+float division (float a, float b){
+    return a/b;
+}
 // methode main par fatou
-
+main(){
+    
+}
 
 
 
