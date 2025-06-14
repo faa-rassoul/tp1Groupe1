556dfd68a6bfca78277ca2a91f861c9ecef4f194
faa-rassoul <fatdiopgningue@gmail.com>
2025-06-14 12:11:09 +0100

    Create calculatrice.c

---
+++ calculatrice.c
@@ -0,0 +1,28 @@
+// directives de compilation djiby
+
+
+// fontion addition par marone
+
+
+
+// fontion soustraction par marone
+
+
+
+// fontion multiplication par djiby
+
+
+
+// fontion division par fatou
+
+
+
+// methode main par fatou
+
+
+
+
+
+
+
+
